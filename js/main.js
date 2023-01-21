7dcddb4cdc90da6bc8ffe1a2ce56c5828dd546c6
gracegesalan <113370993+gracegesalan@users.noreply.github.com>
2023-01-21 15:29:06 -0500

    Edited JS and html

--- js/main.js
+++ js/main.js
@@ -1,65 +1,62 @@
-console.log('Javascript is running');
+console.log('Javascript is running!');
 
 // const vectorGraphic = document.querySelector('#newBadge');
 
 let LocationIcon = document.querySelector('#LocationIcon');
 let GlobeIcon = document.querySelector('#GlobeIcon');
 let HouseIcon = document.querySelector('#HouseIcon');
-let WallClockIcon = document.querySelector('#WallClockicon');
-let EnvelopeIcon = document.querySelector('#Envelope');
-let LocationArrowIcon = document.querySelector('#LocationArrowIcons');
+let WallClockIcon = document.querySelector('#WallClockIcon');
+let EnvelopeIcon = document.querySelector('#EnvelopeIcon');
+let LocationArrowIcon = document.querySelector('#LocationArrowIcon');
 let SmartphoneIcon = document.querySelector('#SmartphoneIcon');
-let MagnifyingGlassIcon = document.querySelector('#MagnifyingGlassicon');
+let MagnifyingGlassIcon = document.querySelector('#MagnifyingGlassIcon');
 let PowerIcon = document.querySelector('#PowerIcon');
 let CheckMarkIcon = document.querySelector('#CheckMarkIcon');
 
-const vectorGraphic = document.querySelector('#newBadge');
 
-function logID() {
-    console.log(this.id);
-}
 
-function LocationIcon() {
+
+
+function LocationIcons() {
     console.log("You have clicked the Location Icon");
 }
-function GlobeIcon() {
+function GlobeIcons() {
     console.log("You have clicked the Globe Icon");
 }
-function HouseIcon() {
+function HouseIcons() {
     console.log("You have clicked the House Icon");
 }
-function WallClockIcon() {
+function WallClockIcons() {
     console.log("You have clicked the Wall Clock Icon");
 }
-function EnvelopeIcon() {
+function EnvelopeIcons() {
     console.log("You have clicked the Envelope Icon");
 }
-function LocationArrowIcon() {
+function LocationArrowIcons() {
     console.log("You have clicked the Location Arrow Icon");
 }
-function SmartphoneIcon() {
+function SmartphoneIcons() {
     console.log("You have clicked the Smartphone Icon");
 }
-function MagnifyingGlassIcon() {
+function MagnifyingGlassIcons() {
     console.log("You have clicked the Magnifying Glass Icon");
 }
-function PowerIcon() {
+function PowerIcons() {
     console.log("You have clicked the Power Icon");
 }
-function CheckMarkIcon() {
+function CheckMarkIcons() {
     console.log("You have clicked the Check Mark Icon");
 }
 
 
-
-LocationIcon.addEventListener('click' , logID);
-GlobeIcon.addEventListener('click' , GlobeIcon);
-HouseIcon.addEventListener('click' , HouseIcon);
-WallClockIcon.addEventListener('click' , WallClockIcon);
-EnvelopeIcon.addEventListener('click' , EnvelopeIcon);
-LocationArrowIcon.addEventListener('click' , LocationArrowIcon);
-SmartphoneIcon.addEventListener('click' , SmartphoneIcon);
-MagnifyingGlassIcon.addEventListener('click' , MagnifyingGlassIcon);
-PowerIcon.addEventListener('click' , PowerIcon);
-CheckMarkIcon.addEventListener('click' , CheckMarkIcon);
+LocationIcon.addEventListener('click' , LocationIcons);
+GlobeIcon.addEventListener('click' , GlobeIcons);
+HouseIcon.addEventListener('click' , HouseIcons);
+WallClockIcon.addEventListener('click' , WallClockIcons);
+EnvelopeIcon.addEventListener('click' , EnvelopeIcons);
+LocationArrowIcon.addEventListener('click' , LocationArrowIcons);
+SmartphoneIcon.addEventListener('click' , SmartphoneIcons);
+MagnifyingGlassIcon.addEventListener('click' , MagnifyingGlassIcons);
+PowerIcon.addEventListener('click' , PowerIcons);
+CheckMarkIcon.addEventListener('click' , CheckMarkIcons);
 
